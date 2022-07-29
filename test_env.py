ac6c3036bb93d2bc1707d655f8fa68937df69d0c
mahoushisaber@gmail.com <mahoushisaber@gmail.com>
2022-07-29 15:21:15 -0700

    Demo for playing the game with 2 players
    - added model retrained for 10 million steps

--- test_env.py
+++ test_env.py
@@ -4,16 +4,16 @@ from pettingzoo.test import parallel_api_test, performance_benchmark, test_save_
 
 env_kwargs = dict(control=True, render_display=True, frame_rate=60, max_steps=60 * 10, observation_type="rgb", player_speed=10, slowdown_factor = 2.0)
 #change
-env = aec_env(**env_kwargs)
-env.reset()
-while True:
-    env.step(None)
-    env.render()
-# env = parallel_env(**env_kwargs)
-# parallel_api_test(env)
 # env = aec_env(**env_kwargs)
-
-# performance_benchmark(env)a
 # env.reset()
-# test_save_obs(env)
-# random_demo(env)
+# while True:
+#     env.step(None)
+#     env.render()
+env = parallel_env(**env_kwargs)
+parallel_api_test(env)
+env = aec_env(**env_kwargs)
+
+performance_benchmark(env)
+env.reset()
+test_save_obs(env)
+random_demo(env)
